9651c4c196f2d17fca8bb304196bbbbeed8f48c6
James Sederia <jjsederia08@gmail.com>
2026-07-30 11:20:32 +0800

    Feat: social links

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -13,7 +13,7 @@ export default function Contact() {
     }
     
     if (submitted) { 
-        return <p>Thanks, {form.userName}!</p>
+        return <p className="form-sub-confirm">Thanks, {form.userName}!</p>
     }
     return (    
         <div form-container>
@@ -45,13 +45,23 @@ export default function Contact() {
             <button className="submit-btn" type="submit">Send</button>
             </div>
         </form>
-         <h2 className="header-landing">My other socials</h2>
-         <div className="icon-container">
-            <p>Facebook</p>
-            <p>Instagram</p>
-            <p>LinkedIn</p>
-            <p>Github</p>
-         </div>
+         <h2 className="header-landing">- Check out my other socials -</h2>
+         <div className="socials-container">
+             <div className="socials">
+                <a href="https://www.facebook.com/share/19DfdSR2J1/" target="_blank" rel="noopener noreferrer" className="social-name">
+                    Facebook
+                </a>
+                <a href="https://www.instagram.com/_jvmszn?igsh=M25oZXR0OWQ2Z2F5" target="_blank" rel="noopener noreferrer" className="social-name">
+                    Instagram
+                </a>
+                <a href="https://www.linkedin.com/in/james-sederia-4b87383a8?utm_source=share_via&utm_content=profile&utm_medium=member_android" target="_blank" rel="noopener noreferrer" className="social-name">
+                    LinkedIn
+                </a>
+                <a href="https://github.com/james-sed" target="_blank" rel="noopener noreferrer" className="social-name">
+                    Github
+                </a>
+        </div>
+</div>
         </div> 
 )
 }
