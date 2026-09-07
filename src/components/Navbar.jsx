9bfd20dcd9f685861b4260470c2a71932f78990a
James Sederia <jjsederia08@gmail.com>
2026-09-07 18:57:05 +0800

    Nav bar redesign

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,42 @@
-import { NavLink } from "react-router-dom";
-import { Link } from "react-router-dom";
+import { NavLink, Link } from "react-router-dom";
+import { useState } from "react";
+import { HiMenu, HiX } from "react-icons/hi";
 
 export default function Navbar() {
-    return (
-        <div className="nav-bar-container">
-            <nav className="Nav-bars">
-            <NavLink to="/" end>Home</NavLink>
-            <NavLink to="/about">About</NavLink>
-            <NavLink to="/contact">Contact</NavLink>
-            <NavLink to="/projects">Projects</NavLink>
-        </nav>
-        </div>
-        
-)
-}
+  const [menuOpen, setMenuOpen] = useState(false);
 
+  const closeMenu = () => setMenuOpen(false);
+
+  return (
+    <header className="navbar">
+      <div className="navbar-inner">
+        <Link to="/" className="navbar-wordmark" onClick={closeMenu}>
+          Sederia
+        </Link>
+
+        <button
+          className={`navbar-toggle ${menuOpen ? "is-open" : ""}`}
+          onClick={() => setMenuOpen((o) => !o)}
+          aria-label="Toggle navigation"
+        >
+          {menuOpen ? <HiX size={24} /> : <HiMenu size={24} />}
+        </button>
+
+        <nav className={`navbar-links ${menuOpen ? "is-open" : ""}`}>
+          <NavLink to="/" end onClick={closeMenu}>
+            Home
+          </NavLink>
+          <NavLink to="/about" onClick={closeMenu}>
+            About
+          </NavLink>
+          <NavLink to="/projects" onClick={closeMenu}>
+            Projects
+          </NavLink>
+          <NavLink to="/contact" onClick={closeMenu}>
+            Contact
+          </NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
